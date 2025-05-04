1e035b9419b2d3f110c429b738e64570425e2e9b
jforbes02 <131290572+jforbes02@users.noreply.github.com>
2025-05-04 18:44:22 -0400

    Created Database using postgres and SQLALCHEMY

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from flask import Flask, request, session
-from flask_login import logout_user, login_required, current_user, LoginManager, login_user
-from database import db, Config, User, Players, Bets
+from flask import Flask, sessions
+from flask_login import LoginManager, UserMixin, current_user, LoginManager, UserMixin, login_user
+from database import db, Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQL_DATABASE_URI
-
+login_manager = LoginManager()
+login_manager.init_app(app)
+#login_manager.login_view = 'login'
 @app.route('/')
 def home():  # put application's code here
     return 'Hello World!'
 
-@app.route('/register', methods=['GET', 'POST'])
-def registration():
-    return 'register'
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- database.py
+++ database.py
@@ -1,34 +1,78 @@
-import os
-
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 db = SQLAlchemy()
 
 class Config:
-    SQL_DATABASE_URI = 'sqlite:///database.db'
-    SQLAlchemy_TRACK_Modify = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Coolpyro55@localhost:5432/sportsbetting'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+
     wallet = db.Column(db.Integer, nullable=False, default=0.0)
     wins = db.Column(db.Integer, nullable=False, default=0)
     losses = db.Column(db.Integer, nullable=False, default=0)
     transactions = db.Column(db.Integer, nullable=False, default=0)
+    bets = db.relationship('Bet', backref='user', lazy='select')
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    def remove(self):
+        db.session.delete(self)
 
-class Bets(db.Model):
+class Player(db.Model):
+    __tablename__ = 'players'
     id = db.Column(db.Integer, primary_key=True)
-    odds = db.Column(db.Integer, nullable=False)
-    name = db.Column(db.String, nullable=False) #parlay, sgp, straight
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    number = db.Column(db.String(80), nullable=False)
+    position = db.Column(db.String(15), nullable=False)
+    season = db.Column(db.String(10), nullable=False)
+    avgPoints = db.Column(db.Numeric(4, 1), nullable=False)
+    avgRebounds = db.Column(db.Numeric(4, 1), nullable=False)
+    avgAssists = db.Column(db.Numeric(4, 1), nullable=False)
+    avgSteals = db.Column(db.Numeric(3, 1), nullable=False)
+    avgBlocks = db.Column(db.Numeric(3, 1), nullable=False)
+    avgTurnovers = db.Column(db.Numeric(3, 1), nullable=False)
+    fgPercentage = db.Column(db.Numeric(3, 1), nullable=False)
 
+    team = db.relationship('Teams', backref='players')
+    def __repr__(self):
+        return f'<Player {self.name} ({self.team.inShort})>'
 
-class Players(db.Model):
+class Team(db.Model):
+    __tablename__ = 'teams'
     id = db.Column(db.Integer, primary_key=True)
-    team = db.Column(db.String, nullable=False)
-    name = db.Column(db.String, nullable=False)
-    number = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
+    inShort = db.Column(db.String(4), nullable=False, unique=True)
+    location = db.Column(db.String(25), nullable=False)
+    wins = db.Column(db.Integer, nullable=False, default=0)
+    losses = db.Column(db.Integer, nullable=False, default=0)
+
+    players = db.relationship('Players', backref='teams')
+    def __repr__(self):
+        return f'<Team {self.inShort}>'
 
 
+class Bet(db.Model):
+    __tablename__ = 'bets'
 
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    type = db.Column(db.String(20), nullable=False) #SGP, Straights, Legs etc
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    odds = db.Column(db.Numeric(6, 1), nullable=False)
+
+    def __repr__(self):
+        return f'<Bet {self.id} by {self.user.username}>'
